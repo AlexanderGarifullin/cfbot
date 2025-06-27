e3b8b47dabd8a53a07a2a9dce01bf68ef224ce1a
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2025-06-28 02:10:02 +0500

    Initial commit

---
+++ src/main/java/com/ga/cfbot/CfbotApplication.java
@@ -0,0 +1,13 @@
+package com.ga.cfbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/cfbot/CfbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.cfbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
